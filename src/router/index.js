4da67ab77b54ab552540f2ef9e8972be3815b593
zk <iamzhengkai@163.com>
2021-04-13 17:10:23 +0800

    关闭prettier

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    redirect: "login",
+  },
+    {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import("../views/Login/index.vue"),
   },
   {
     path: "/about",
@@ -14,7 +21,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import("../views/About.vue"),
   },
 ];
 
---
+++ src/views/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+const app = {
+  el: '#app',
+  data() {
+    return {
+      message: 'Hello Vue'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
